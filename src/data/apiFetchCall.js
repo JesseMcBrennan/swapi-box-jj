29834598414b8bfb681097524c49d473b8f1b14b
Joel O'Connor <joconno07@gmail.com>
2018-08-20 17:59:04 -0600

    cleaned up linting errors

--- src/data/apiFetchCall.js
+++ src/data/apiFetchCall.js
@@ -1,20 +1,20 @@
 
 export const filmDataCall = async (url) => {
-const filmUrl = "https://swapi.co/api/films/"
-  const response = await fetch(filmUrl)
-  const rawFilmData = await response.json()
-  const cleanedData = await cleanFilmData(rawFilmData.results)
-  return cleanedData
-  }
+  const filmUrl = "https://swapi.co/api/films/";
+  const response = await fetch(filmUrl);
+  const rawFilmData = await response.json();
+  const cleanedData = await cleanFilmData(rawFilmData.results);
+  return cleanedData;
+};
 
 
 const cleanFilmData = (rawFilmData) => {
-   return rawFilmData.map(film => ({
-        title: film.title,
-        crawl: film.opening_crawl,
-        releaseDate: film.release_date
-      })
-   )
-}
+  return rawFilmData.map(film => ({
+    title: film.title,
+    crawl: film.opening_crawl,
+    releaseDate: film.release_date
+  })
+  );
+};
 
 
