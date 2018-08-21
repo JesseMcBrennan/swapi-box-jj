b3f53a5ae8376ba860065b1392d029ad3187c8bc
Joel O'Connor <joconno07@gmail.com>
2018-08-21 17:06:15 -0600

    built out navbar so that it passed the button names up to app, added prop type

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import './styles.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
 
-export const NavBar = () => {
-  return(
+export const NavBar = ({ handleClick }) => {
+  const click = (event) => handleClick(event.target.name);
+
+  return (
     <div className="navBar">
-      <button>People</button>
-      <button>Vehicles</button>
-      <button>Planets</button>
+      <button onClick={click} name='people'>People</button>
+      <button onClick={click} name='vehicles'>Vehicles</button>
+      <button onClick={click} name='planets'>Planets</button>
+      <button onClick={click} name='favorites'>favorites</button>
     </div>
-  )
-}
+  );
+};
+
+NavBar.propTypes = {
+  handleClick: PropTypes.func
+};
