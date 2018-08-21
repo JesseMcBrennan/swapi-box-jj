00e86d78c24aa9bd360cb679e3c98a2401b38cc5
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-20 18:40:07 -0600

    Add initial styling to App.js, IntroContainer and NavBar

--- src/Components/IntroContainer/index.js
+++ src/Components/IntroContainer/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropType from 'prop-types';
+import './styles.css'
 
 export const IntroContainer = ({filmData}) => {
   const randomNumber = Math.floor(Math.random() * (7));
   const displayFilmData = filmData[`${randomNumber}`];
 
   return (
-    <div>
+    <div className="introContainer">
       <p>{displayFilmData.crawl}</p>
       <h1>{displayFilmData.title}</h1>
       <h2>{displayFilmData.releaseDate}</h2>
