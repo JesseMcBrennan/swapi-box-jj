5e4e01421f0d0f7e5749776e578c32c9018d6bef
Joel O'Connor <joconno07@gmail.com>
2018-08-21 16:32:41 -0600

    structured jsx for scrolling animation of film text

--- src/Components/IntroContainer/index.js
+++ src/Components/IntroContainer/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropType from 'prop-types';
-import './styles.css'
+import './styles.css';
 
 export const IntroContainer = ({filmData}) => {
   const randomNumber = Math.floor(Math.random() * (7));
   const displayFilmData = filmData[`${randomNumber}`];
 
   return (
-    <div className="introContainer">
-      <p>{displayFilmData.crawl}</p>
-      <h1>{displayFilmData.title}</h1>
-      <h2>{displayFilmData.releaseDate}</h2>
+    <div className='target'>
+      <div className="introContainer">
+        <p>{displayFilmData.crawl}</p>
+        <h1>{displayFilmData.title}</h1>
+        <h2>{displayFilmData.releaseDate}</h2>
+      </div>
     </div>
   );
 }; 
