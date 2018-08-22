c7241f84ddbb5a5c8014f7e52fc8dbd3a9ea82a3
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-21 18:43:19 -0600

    Add working iteration-one cleanPeople data functionality

--- src/data/apiFetchCall.js
+++ src/data/apiFetchCall.js
@@ -1,4 +1,4 @@
-export const filmDataCall = async (url) => {
+export const getFilmData = async (url) => {
   const filmUrl = "https://swapi.co/api/films/";
   const response = await fetch(filmUrl);
   const rawFilmData = await response.json();
@@ -14,3 +14,44 @@ const cleanFilmData = (rawFilmData) => {
   })
   );
 };
+
+export const getPeopleData = async (url) =>{
+  const peopleUrl = "https://swapi.co/api/people/"
+  const response = await fetch(peopleUrl);
+  const rawPeopleData = await response.json();
+  return await cleanPeopleData(rawPeopleData.results);
+
+}
+
+const cleanPeopleData = async (rawPeopleData) => {
+  const unresolvedPromiseArray = rawPeopleData.map(async person => {
+    const name = person.name
+    const worldUrl = person.homeworld;
+    const speciesUrl = person.species[0];
+    const receivedWorldData = await getWorldData(worldUrl);
+    const receivedSpeciesData = await getSpeciesData(speciesUrl);
+    const peopleObj = {name,...receivedWorldData,...receivedSpeciesData};
+    return peopleObj;
+  })
+
+  return await Promise.all(unresolvedPromiseArray)
+  
+}
+
+const getWorldData = async (worldUrl) => {
+  const response = await fetch(worldUrl);
+  const worldData = await response.json();
+  return {
+    planet: worldData.name,
+    population: worldData.population
+  }
+}
+
+const getSpeciesData = async (speciesUrl) => {
+  const response = await fetch(speciesUrl);
+  const speciesData = await response.json();
+
+  return {
+    species: speciesData.name
+  }
+}
