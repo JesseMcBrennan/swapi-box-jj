eadbf28a44d6d258852be58983ee9ed2968a411a
Joel O'Connor <joconno07@gmail.com>
2018-08-22 14:43:26 -0600

    linted fetch file

--- src/data/apiFetchCall.js
+++ src/data/apiFetchCall.js
@@ -15,24 +15,24 @@ const cleanFilmData = (rawFilmData) => {
 };
 
 export const getPeopleData = async (url) =>{
-  const peopleUrl = "https://swapi.co/api/people/"
+  const peopleUrl = "https://swapi.co/api/people/";
   const response = await fetch(peopleUrl);
   const rawPeopleData = await response.json();
   return await cleanPeopleData(rawPeopleData.results);
-}
+};
 
 const cleanPeopleData = async (rawPeopleData) => {
   const unresolvedPromiseArray = rawPeopleData.map(async person => {
-    const name = person.name
+    const name = person.name;
     const worldUrl = person.homeworld;
     const speciesUrl = person.species[0];
     const receivedWorldData = await getWorldData(worldUrl);
     const receivedSpeciesData = await getSpeciesData(speciesUrl);
-    const peopleObj = {name,...receivedWorldData,...receivedSpeciesData};
+    const peopleObj = {name, ...receivedWorldData, ...receivedSpeciesData};
     return peopleObj;
-  })
-  return await Promise.all(unresolvedPromiseArray)
-}
+  });
+  return await Promise.all(unresolvedPromiseArray);
+};
 
 const getWorldData = async (worldUrl) => {
   const response = await fetch(worldUrl);
@@ -40,13 +40,21 @@ const getWorldData = async (worldUrl) => {
   return {
     planet: worldData.name,
     population: worldData.population
-  }
-}
+  };
+};
 
 const getSpeciesData = async (speciesUrl) => {
   const response = await fetch(speciesUrl);
   const speciesData = await response.json();
   return {
     species: speciesData.name
-  }
-}
+  };
+};
+
+export const getVehicleData = async (url) => {
+  console.log('get vehicle data called');
+};
+
+export const getPlanetData = async (url) => {
+  console.log('get planet data called');
+};
