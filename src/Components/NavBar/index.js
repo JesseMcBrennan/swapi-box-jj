6a1e19ae4f6f0a6e147a560019d7818a2327362d
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-22 16:01:08 -0600

    Add ternary to change className when selected

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -2,15 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
 
-export const NavBar = ({ handleClick }) => {
+export const NavBar = ({ handleClick, selectedButton }) => {
   const click = (event) => handleClick(event.target.name);
 
+
   return (
-    <div className="navBar">
-      <button onClick={click} name='people'>People</button>
-      <button onClick={click} name='vehicles'>Vehicles</button>
-      <button onClick={click} name='planets'>Planets</button>
-      <button onClick={click} name='favorites'>favorites</button>
+    <div className="navBar" >
+      <button 
+        onClick={click} 
+        name='people' 
+        className={selectedButton === 'people' ? 'selected' : 'unselected'}
+        >People
+      </button>
+      <button 
+        onClick={click} 
+        name='vehicles' 
+        className={selectedButton === 'vehicles' ? 'selected' : 'unselected'}
+        >Vehicles
+      </button>
+      <button 
+        onClick={click} 
+        name='planets' 
+        className={selectedButton === 'planets' ? 'selected' : 'unselected'}
+        >Planets
+      </button>
+      <button 
+        onClick={click} 
+        name='favorites' 
+        className={selectedButton === 'favorites' ? 'selected' : 'unselected'}
+        >Favorites
+      </button>
     </div>
   );
 };
