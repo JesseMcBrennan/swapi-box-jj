13f0000cfb2089c282fd613df8068c342e98cfe0
Joel O'Connor <joconno07@gmail.com>
2018-08-21 20:31:57 -0600

    built card container and proptype validation

--- src/Components/CardContainer/index.js
+++ src/Components/CardContainer/index.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card } from "../Card";
 import './styles.css';
+
+export const CardContainer = ({ peopleData }) => {
+  const displayPeopleCards = peopleData.map(person => <Card {...person} />);
+
+  return (
+    <div>
+      {displayPeopleCards}
+    </div>
+  );
+};
+
+CardContainer.propTypes = {
+  peopleData: PropTypes.array
+};
