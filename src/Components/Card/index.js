0b0d70b98e9c3bc0a6b2d1935d914b7c752efbb7
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-23 13:51:37 -0600

    Pass correct data from Card to App

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -6,54 +6,45 @@ import './styles.css';
 export const Card = ({
   addFavorite,
   selected,
-  name,
-  species,
-  population,
-  planet,
-  terrain,
-  climate,
-  residents,
-  model,
-  passengers,
-  vehicleClass
+  data
 }) => {
   switch (selected) {
     case 'people':
       return (
-        <div className='card' onClick={() => addFavorite()}>
-          <h2>{name}</h2>
-          <h2>Homeworld: {planet}</h2>
-          <h2>Species: {species}</h2>
-          <h2>Population: {population}</h2>
+        <div className='card' onClick={() => addFavorite(data)}>
+          <h2>{data.name}</h2>
+          <h2>Homeworld: {data.planet}</h2>
+          <h2>Species: {data.species}</h2>
+          <h2>Population: {data.population}</h2>
         </div>
       );
-    case 'planets':
-      const displayResidents = residents.map(resident =><li>{resident}</li>);
-      return (
-        <div className='card' onClick={() => addFavorite()}>
-          <h2>{name}</h2>
-          <h2>Terrain: {terrain}</h2>
-          <h2>Population: {population}</h2>
-          <h2>Climate: {climate}</h2>
-          <h2>Residents:<ul>{displayResidents}</ul></h2>
-        </div>
-      );
-    case 'vehicles':
-      return (
-        <div className='card' onClick={() => addFavorite()}>
-          <h2>{name}</h2>
-          <h2>Model: {model}</h2>
-          <h2>Passengers: {passengers}</h2>
-          <h2>Class: {vehicleClass}</h2>
-        </div>
-      );
-    case 'favorites':
-      return (
-        <div className='card' onClick={() => addFavorite()}>
-          <h1>put favorites here</h1>
-        </div>
-      );
-    default:
+    // case 'planets':
+    //   const displayResidents = residents.map(resident =><li>{resident}</li>);
+    //   return (
+    //     <div className='card' onClick={() => addFavorite()}>
+    //       <h2>{name}</h2>
+    //       <h2>Terrain: {terrain}</h2>
+    //       <h2>Population: {population}</h2>
+    //       <h2>Climate: {climate}</h2>
+    //       <h2>Residents:<ul>{displayResidents}</ul></h2>
+    //     </div>
+    //   );
+    // case 'vehicles':
+    //   return (
+    //     <div className='card' onClick={() => addFavorite()}>
+    //       <h2>{name}</h2>
+    //       <h2>Model: {model}</h2>
+    //       <h2>Passengers: {passengers}</h2>
+    //       <h2>Class: {vehicleClass}</h2>
+    //     </div>
+    //   );
+    // case 'favorites':
+    //   return (
+    //     <div className='card' onClick={() => addFavorite()}>
+    //       <h1>put favorites here</h1>
+    //     </div>
+    //   );
+      default:
       break;
   }
 };
