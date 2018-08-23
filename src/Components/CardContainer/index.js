dfe7dd6cae1194f824c13b2dc723b514d6d44b59
Joel O'Connor <joconno07@gmail.com>
2018-08-23 12:39:54 -0600

    cardcontainer update to handle conditional render and diffrent props

--- src/Components/CardContainer/index.js
+++ src/Components/CardContainer/index.js
@@ -3,15 +3,22 @@ import PropTypes from 'prop-types';
 import { Card } from "../Card";
 import './styles.css';
 
-export const CardContainer = ({ cardData }) => {
-  const displayPeopleCards = cardData.map(data => <Card {...data} />);
+export const CardContainer = ({ cardData, selected }) => {
+  const displayPeopleCards = cardData.map(data => (
+    <Card 
+      key={Date.now() * Math.random()} 
+      {...data} 
+      selected={selected} 
+    />)
+  );
   return (
-    <div>
+    <div className='card-container'>
       {displayPeopleCards}
     </div>
   );
 };
 
 CardContainer.propTypes = {
-  cardData: PropTypes.array
+  cardData: PropTypes.array,
+  selected: PropTypes.string
 };
