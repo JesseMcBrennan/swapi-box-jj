51215844938e33ff0558f91b79edb65e354089ac
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-26 17:08:13 -0600

    Add toggleFavorites test

--- src/tests/Card.test.js
+++ src/tests/Card.test.js
@@ -21,14 +21,29 @@ describe('Card', () => {
   });
 
   it('the list items should change depending on what card is rendering', () => {
-    mockPerson = {name: "Luke Skywalker", planet: "Tatooine", species: "Human", population: "200000"}
 
-    const mockEvent = {target:{name: 'people'}}
+    const mockEvent = {target: {name: 'people'}}
 
-    wrapper.find('[name="people"]').simulate('click', mockEvent)
-
-
-
+    wrapper.find('.card').simulate('click', mockEvent)
+    expect(wrapper.find('.card').length).toEqual(1) 
   })
+
+  it('should render the correct card depending on what button is pressed', () => {
+    const mockEvent = {target: {name: 'people'}}
+
+    wrapper.find('.people').simulate('click', mockEvent)
+    expect(wrapper.find('.card').hasClass('people'))
+  })
+
+  it('should call the toggleFavorites method with the correct parameters', () => {
+    const mockEvent = {target: {name: 'people'}}
+
+    wrapper.find('.card').simulate('click', mockEvent)
+    expect(mockToggleFavorites).toHaveBeenCalledWith(mockData)
+    console.log()
+  })
+
+
+
 
 });
